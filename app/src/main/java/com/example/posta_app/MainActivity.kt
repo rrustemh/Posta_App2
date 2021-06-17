d7cbb620c090f359735cee160e5b6dc9c22d4efa
rrustemh <58404371+rrustemh@users.noreply.github.com>
2021-06-17 22:37:24 +0200

    Adding fragments and navigation graphs for user navigation and databinding

--- app/src/main/java/com/example/posta_app/MainActivity.kt
+++ app/src/main/java/com/example/posta_app/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.posta_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.posta_app.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private var navController: NavController? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupNavigationComponent()
+        val bottomNavBar = findViewById<BottomNavigationView>(R.id.user_bottom_navigation)
+        bottomNavBar.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.ic_dashboard -> navController?.navigate(R.id.dashboardFragment)
+                R.id.ic_add -> navController?.navigate(R.id.deliveryFragment)
+                R.id.ic_profile -> navController?.navigate(R.id.profileFragment)
+            }
+            true
+        }
+    }
+    private fun setupNavigationComponent() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
